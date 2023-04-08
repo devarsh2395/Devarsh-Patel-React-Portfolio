6a2589ddcfaa57ae1b0de6880188cdd4adfe19b5
devarsh2395 <devarsh2395@gmail.com>
2023-04-08 14:01:38 -0400

    Updated code

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,34 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 
 function Navigation(props) {
- const {
-  pages = [],
-  setCurrentPage, 
-  currentPage
- } = props
+  const { pages = [], setCurrentPage, currentPage } = props;
 
- useEffect(() => {
-  document.title = currentPage.name;
- }, [currentPage])
+  useEffect(() => {
+    document.title = currentPage.name;
+  }, [currentPage]);
+
   return (
-    <nav className="navbar navbar-expand-md navbar-light bg-light">
-  <div className="container-fluid">
-    <ul className="navbar-nav ms-auto mb-2 mb-md-0">
-      {pages.map((Page) => (
-        <li className={`nav-item ${currentPage.name === Page.name && 'active'}`} key={Page.name}>
-          <span className="nav-link" onClick={() => setCurrentPage(Page)}>
-            {Page.name}
-          </span>
-        </li>
-      ))}
-    </ul>
-  </div>
-</nav>
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <div className="container-fluid">
+        <ul className="navbar-nav ms-auto mb-2 mb-md-0">
+          {pages.map((Page) => (
+            <li
+              className={`nav-item ${
+                currentPage.name === Page.name && "active"
+              }`}
+              key={Page.name}
+            >
+              <span
+                className="nav-link"
+                onClick={() => setCurrentPage(Page)}
+              >
+                {Page.name}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
   );
 }
 
